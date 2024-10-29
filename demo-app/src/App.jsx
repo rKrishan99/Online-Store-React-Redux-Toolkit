cf28369fdceaa6204b51c6134887b1a376863acd
RajithaKWG <rkrishan894@gmail.com>
2024-10-29 15:33:49 +0530

    Create componnets

--- demo-app/src/App.jsx
+++ demo-app/src/App.jsx
@@ -1,13 +1,27 @@
-import Product from "./components/product/Product"
-import 'bootstrap/dist/css/bootstrap.min.css'
+import Product from "./components/product/Product";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Routes>
+        <Route></Route>
+      </Routes>
+    )
+  );
 
   return (
     <>
-      <Product/>
+      <Product />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
